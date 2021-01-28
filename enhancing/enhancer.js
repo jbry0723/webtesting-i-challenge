2fb7bc3e3c4db8e6da3d17661f2966cd3b5bf655
Justin Justin <jbry0723@gmail.com>
2021-01-27 20:20:21 -0500

    finished MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,25 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement<=19)
+  {item.enhancement+=1}
+  else {null}
+  return item
 }
 
 function fail(item) {
-  return { ...item };
+ if (item.enhancement<=14)
+ {item.durability-=5}
+if(item.enhancement>=15)
+{item.durability-=10}
+if(item.enhancement>=16)
+{item.enhancement-=1}
+ return item
 }
 
 function repair(item) {
-  return { ...item };
+  item.durability=100
+  return item;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,49 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+
+describe("enhancer tests", () => {
+  let item;
+  beforeEach(() => {
+    item = {
+      name: "wizards hat",
+      enhancement: 19,
+      durability: 30,
+    };
+  });
+  it("repair functions", () => {
+    enhancer.repair(item);
+    expect(item.durability).toEqual(100);
+  });
+  it("success returns expected values", () => {
+    enhancer.success(item);
+    expect(item.enhancement).toEqual(20);
+    expect(item.durability).toEqual(30);
+    enhancer.success(item);
+    expect(item.enhancement).toEqual(20);
+  });
+  it("failure with enhancement>15 returns expected values", () => {
+    item = {
+      ...item,
+      enhancement: 13,
+    };
+
+    enhancer.fail(item);
+    expect(item.durability).toEqual(25);
+  });
+  it("failure with enhancement 15 returns expected value", () => {
+    item = {
+      ...item,
+      enhancement: 15,
+    };
+    enhancer.fail(item);
+    expect(item.durability).toEqual(20);
+  });
+  it("failure with enhancement 17+ returns expected value", () => {
+    item = {
+      ...item,
+      enhancement: 17,
+    };
+    enhancer.fail(item);
+    expect(item.durability).toEqual(20);
+    expect(item.enhancement).toEqual(16)
+  });
+});
